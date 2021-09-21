14498e7921090fdb41ad88ac00328685de855942
Ondrej Polesny <ondrejp@kentico.com>
2021-09-21 12:06:32 +0200

    Update gatsby-config.js
    Updates the texts according to new Kontent brand guidelines.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby starter site with Kentico Kontent`,
+    title: `Gatsby starter site with Kontent by Kentico`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
